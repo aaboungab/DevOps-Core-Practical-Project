58bd1a2b915d75967c21de78b6a46e45f8d93acf
Anas Aboungab <aaboungab@qa.com>
2020-12-03 15:42:13 +0000

    updated

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,3 @@
 #! /bin/bash
-
-if (env.rollback == 'false'){
-    image1 = docker.build("aaboungab/service1")
-    image2 = docker.build("aaboungab/service2")
-    image3 = docker.build("aaboungab/service3")
-    image4 = docker.build("aaboungab/service4")
-    imagesql = docker.build("aaboungab/mysql")
-    imagenginx = docker.build("aaboungab/nginx")}
+sudo docker-compose down --rmi all
+sudo docker-compose build
