d38ed3e479dff97ffdd5fb18c47dc7d4aa7d3107
Anas Aboungab <aaboungab@qa.com>
2020-12-03 15:32:59 +0000

    updated

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 if (env.rollback == 'false'){
-                            image1 = docker.build("aaboungab/service1")
-                            image2 = docker.build("aaboungab/service2")
-                            image3 = docker.build("aaboungab/service3")
-                            image4 = docker.build("aaboungab/service4")
-                            imagesql = docker.build("aaboungab/mysql")
-                            imagenginx = docker.build("aaboungab/nginx")}
+    image1 = docker.build("aaboungab/service1")
+    image2 = docker.build("aaboungab/service2")
+    image3 = docker.build("aaboungab/service3")
+    image4 = docker.build("aaboungab/service4")
+    imagesql = docker.build("aaboungab/mysql")
+    imagenginx = docker.build("aaboungab/nginx")}
