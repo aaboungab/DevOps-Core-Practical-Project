e7ae9b7e51b0511e3544f344dd9ecde6b3002688
Anas Aboungab <aaboungab@qa.com>
2020-12-03 14:01:15 +0000

    updated

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-docker login
 docker-compose down --rmi all
 docker-compose build
+docker.withRegistry('https://registry.hub.docker.com', 'DOCKERHUB_CREDENTIALS'){
+    image.push("${env.app_version}")}
 docker-compose push
