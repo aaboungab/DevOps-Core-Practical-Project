b2e3d93c04e0d09cbf35cbe78d1db6f42588f24e
Anas Aboungab <aaboungab@qa.com>
2020-12-03 11:58:20 +0000

    complete

--- service1/application/__init__.py
+++ service1/application/__init__.py
@@ -4,7 +4,7 @@ from os import getenv
 
 app = Flask(__name__) 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = mysql+pymysql://root:11@mysql:3306/player_db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:11@mysql:3306/player_db'
 app.config['SECRET_KEY'] = 'MYSTIC'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
