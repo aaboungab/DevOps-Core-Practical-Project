f3d28ab0c2bc9d5030e82c8baff0ca3e62c9a179
Anas Aboungab <aaboungab@qa.com>
2020-12-03 10:37:05 +0000

    updated

--- service1/application/__init__.py
+++ service1/application/__init__.py
@@ -1,5 +1,13 @@
-from flask import Flask
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy 
+from os import getenv
 
-app = Flask(__name__)
+app = Flask(__name__) 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = mysql+pymysql://root:11@mysql:3306/player_db
+app.config['SECRET_KEY'] = 'MYSTIC'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 from application import routes
