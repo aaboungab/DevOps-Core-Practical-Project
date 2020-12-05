501dd3fe660ea26b968d9176f09d52f20bd0f179
aaboungab <aaboungab@qa.com>
2020-12-05 12:20:28 +0000

    updated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 
 ssh -i ~/.ssh/ansible_id_rsa jenkins@leader << EOF
 sudo docker node ls
+git clone https://github.com/aaboungab/W9_-SoloProject.git
 sudo docker stack deploy -c docker-compose.yaml w9app
 EOF
 
