5dac5524c98d29efbf9e5cf21350e8aa4a8ce2ed
aaboungab <aaboungab@qa.com>
2020-12-05 12:52:18 +0000

    updated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,11 @@ ssh -i ~/.ssh/ansible_id_rsa jenkins@leader << EOF
 sudo docker node ls
 git clone https://github.com/aaboungab/W9_-SoloProject.git
 cd W9_-SoloProject
-docker pull aaboungab/service1:s1
-docker pull aaboungab/service2:s2
-docker pull aaboungab/service3:s3
-docker pull aaboungab/service4:s4
+docker login
+sudo docker pull aaboungab/service1:s1
+sudo docker pull aaboungab/service2:s2
+sudo docker pull aaboungab/service3:s3
+sudo docker pull aaboungab/service4:s4
 sudo docker stack deploy -c docker-compose.yaml w9app
 EOF
 
